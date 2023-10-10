87bad9e5774e8d93d573b6814d50b369d8d15499
elnurxo <elnurykh@code.edu.az>
2023-10-10 13:50:15 +0400

    js intro (hoisting, variables, var-let-const)

---
+++ 11. js intro, hoisting, var-let-const, data-types/assets/script/main.js
@@ -0,0 +1,18 @@
+// var, const, let
+
+// var - function scope, redeclare, reassign
+// let - block scope, reassign, 
+// const - block scope
+//hoisting
+
+print();
+
+function print() {
+    console.log('salam');
+}
+
+
+
+
+
+
