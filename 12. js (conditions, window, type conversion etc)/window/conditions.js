2595995cd29c197b80095acd1a5ca9a6f96a9959
elnurxo <elnurykh@code.edu.az>
2023-10-11 13:52:03 +0400

    js conditions, window object, type conversion

---
+++ 12. js (conditions, window, type conversion etc)/window/conditions.js
@@ -0,0 +1,58 @@
+//conditions - shertler
+//if, if else, else if, switch case
+
+// let weekDay = Number(prompt("enter number of the weekday!")); //4
+
+// switch (weekDay) {
+//   case 1:
+//     alert("Monday");
+//     break;
+//   case 2:
+//     alert("Tuesday");
+//   case 3:
+//     alert("Wednesday");
+//     break;
+//   case 4:
+//     alert("Thursday");
+//     break;
+//   case 5:
+//     alert("Friday");
+//     break;
+//   case 6:
+//   case 7:
+//     alert("Weekend");
+//     break;
+//   default:
+   
+//     break;
+// }
+
+// let num1 = 9;
+// let num2 = 11;
+// let result;
+
+//ternary if
+// result = (num1>num2) ? num1 : (num1==num2) ? alert("equal!") : num2;
+
+// if (num1>num2) {
+//     result = num1;
+// }
+// else{
+//     result = num2;
+// }
+
+
+// let username = "elnurxo";
+// let bio = "salam";
+
+// alert(bio ?? "hey there, I'm using WhatsApp");
+
+
+let num1 = 9;
+let num2 = 6;
+let result; //undefined
+
+if (num1>num2)
+    alert(num1);
+else
+    alert(num2);
---
+++ 12. js (conditions, window, type conversion etc)/window/main.js
@@ -0,0 +1,31 @@
+// window
+//reserved keywords let,var,const,window
+
+// let age = prompt("enter your age!");
+
+// window.alert(`your age is ${age}`);
+
+//type conversion
+//truty falsy
+
+// let age = prompt("enter your age?"); //string
+// alert(typeof age);
+// let nextAge = Number(age) + 10; 
+// ++value -> increase, output
+
+
+// age+=1; //age = age + 1;
+
+// alert(`your next age in 10 years is ${nextAge}`); // output -> 23
+
+// "" -> empty string -false
+// " " -> white space -true
+
+
+let arr = [];
+
+alert(arr.length);
+
+if (arr.length) {
+    alert("case is true!");
+}
